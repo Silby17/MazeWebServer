a55b6e11875df4dd2fa0e84ee6bcc835132ee55d
Yossi Silberhaft <yossisilb@gmail.com>
2016-05-16 12:00:00 +0300

    Added the DataBase to the context as well as comments done

--- src/MyServletContextListener.java
+++ src/MyServletContextListener.java
@@ -2,20 +2,36 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-
+/******************************************************************************
+ * This Class will initialize the Contexts and add attributes that will be
+ * able to be accessed throughout the application
+ *****************************************************************************/
 public class MyServletContextListener implements ServletContextListener {
-
+    private DBManager dbManager;
     private ServerConnectionManager serverManager;
 
+
+    /*************************************************************************
+     * Initialization method that will initialize the Server Connection
+     * as well as the DataBase manager
+     * @param servletContextEvent
+     ************************************************************************/
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-        System.out.println("Context initialized");
         ServletContext context = servletContextEvent.getServletContext();
         serverManager = new ServerConnectionManager("127.0.0.1", 80);
-
+        this.dbManager = new DBManager();
+        context.setAttribute("database", dbManager);
         context.setAttribute("connection", serverManager);
+        System.out.println("Context initialized");
     }
 
+
+    /************************************************************************
+     * This method will destroy the context at the end of the running
+     * of the application
+     * @param servletContextEvent
+     ***********************************************************************/
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
         System.out.println("Context destroyed");
