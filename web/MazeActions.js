12e107494c1e8d067d1e3f250661d41a96175d43
NAVAJAVA <nshemoul@gmail.com>
2016-05-24 12:43:13 +0300

    single maze can draw a mazeush by a stringush of 10101010

--- web/MazeActions.js
+++ web/MazeActions.js
@@ -1,11 +1,25 @@
-window.onload = function() {
- var mazeImg = new Image();
- mazeImg.setAttribute("src", "images/maze.png");
-//mazeImg.src = "images/maze.png";
- var c = document.getElementById("mazecanvas");
- var ctx = c.getContext("2d");
- //var img = document.getElementById("maze");
- ctx.drawImage(mazeImg, 10, 10);
+//gets a string and makes a grid out of it.
+function DrawMaze(stringMaze) {
+ var canvas = document.getElementById('myCanvas');
+ var context = canvas.getContext('2d');
+ var x = 470, y = 140, index =0;
+//row
+ for (var i = 0; i < 8; i++) {
+  //go over the col, if 0 draw white cube if 1 draw black cube
+  for (var j = 0; j < 8; j++) {
+    if(stringMaze[index] == '1') {
+    context.fillStyle = "#B6E9FA";
+    context.fillRect(x, y, 40, 40);
+   }
+   else {
+     context.fillStyle = "white";
+     context.fillRect(x, y, 40, 40);
+    }
+   x = x + 40;
+   index++;
+  }
+  y = y + 40;
+  x = 470;
+ }
 }
-
-
+DrawMaze("1010101110100010101010111010001000101011101010101010101110101001");
---
+++ web/SinglePlayer.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+</head>
+<body>
+<canvas id="myCanvas" width="1000" height="1000"></canvas>
+<script src = "MazeActions.js"></script>
+</body>
+</html>
+
+
+
+
+
