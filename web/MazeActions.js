34c7cf7c1179892b1798dbc898abcb98f97320ee
NAVAJAVA <nshemoul@gmail.com>
2016-05-25 12:24:40 +0300

    buttons in single (stiil need to style then a bit) to do: resize pandosh and hidde pandosh when click

--- web/MazeActions.js
+++ web/MazeActions.js
@@ -1,7 +1,13 @@
+var currX = 430;
+var currY = 100;
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+var mazeWidth = 400;
+var mazeHeight = 400;
+var imageObj = new Image();
+
 //gets a string and makes a grid out of it.
 function DrawMaze(stringMaze) {
- var canvas = document.getElementById('myCanvas');
- var context = canvas.getContext('2d');
  context.rect(430,100,400,400);
  context.stroke();
  var x = 430, y = 100, index =0;
@@ -25,4 +31,94 @@ function DrawMaze(stringMaze) {
  }
 }
 
+//place the icon of the player on the maze
+function PlacePlayerIcon(currX ,currY) {
+ imageObj.onload = function() {
+  context.drawImage(imageObj, currX, currY);
+ };
+ imageObj.src = "images/Icons/panda.png";
+}
+
+function canMoveTo(destX, destY) {
+// var imgData = context.getImageData(destX, destY, 40, 40);
+ //var data = imgData.data;
+ var canMove = 1; // 1 means: player can move
+ // check whether the rectangle would move inside the bounds of the canvas
+ if (destX >= 0 && destX <= mazeWidth - 40 && destY >= 0 && destY <= mazeHeight - 40) {
+  //for (var i = 0; i < 4 * 40 * 40; i += 4) { // look at all pixels
+   //if (data[i] === 0 && data[i + 1] === 0 && data[i + 2] === 0) { // black
+   // canMove = 0; // 0 means: the rectangle can't move
+   // break;
+   //}
+   //else if (data[i] === 0 && data[i + 1] === 255 && data[i + 2] === 0) { // lime: #00FF00
+    //canMove = 2; // 2 means: the end point is reached
+    //break;
+   //}
+ // }
+ }
+ else {
+  canMove = 0;
+ }
+ return canMove;
+}
+
+//show the move if ok
+function nextMove(newX, newY) {
+ var movingAllowed = canMoveTo(newX, newY);
+ // 1 means can move
+ if (movingAllowed === 1) {
+  currX = newX;
+  currY = newY;
+  PlacePlayerIcon(currX, currY);
+ }
+ // 2 means the palyer reached the end point
+ else if (movingAllowed === 2) {
+  context.font = "40px Arial";
+  context.fillStyle = "blue";
+  context.textAlign = "center";
+  context.textBaseline = "middle";
+  context.fillText("Congratulations!", canvas.width / 2, canvas.height / 2);
+  window.removeEventListener("keydown", move, true);
+ }
+}
+
+
+
+function move(e) {
+ var newX;
+ var newY;
+ e = e || window.event;
+ switch (e.keyCode) {
+     // arrow up key
+  case 38:
+   newX = currX;
+   newY = currY - 40;
+   break;
+     // arrow left key
+  case 37:
+   newX = currX - 40;
+   newY = currY;
+   break;
+     // arrow down key
+  case 40:
+   newX = currX;
+   newY = currY + 40;
+   break;
+     // arrow right key
+  case 39:
+   newX = currX + 40;
+   newY = currY;
+   break;
+   nextMove(newX, newY);
+ }
+}
+
+
+function move1(e){
+ imageObj.style.visibility = 'hidden';
+ PlacePlayerIcon(currX+100, currY+100);
+}
 DrawMaze("1010101110100010101010111010001000101011101010101010101110101001110011110011110011110011110011110011");
+PlacePlayerIcon(currX,currY);
+window.addEventListener("keydown", move1);
+
--- web/SinglePlayer.jsp
+++ web/SinglePlayer.jsp
@@ -1,10 +1,22 @@
 <!DOCTYPE HTML>
 <html>
 <head>
+    <link rel="stylesheet" href="css/mainMenu.css">
 </head>
 <body>
 <canvas id="myCanvas" width="1000" height="1000"></canvas>
 <script src = "MazeActions.js"></script>
+<ul div style="float: right">
+    <li >
+        <button type="button"><a href="MainMenu.jsp">Back</a></button>
+    </li>
+    <li >
+        <button type="button"><a href="SinglePlayer.jsp">Reset</a></button>
+    </li>
+    <li >
+        <button type="button"><a href="SinglePlayer.jsp">Suggestion</a></button>
+    </li>
+    </ul>
 </body>
 </html>
 
