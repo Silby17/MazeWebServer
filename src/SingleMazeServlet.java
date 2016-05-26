e45a9fcf126f2d589bf158fc0a00a8cac53e941c
Yossi Silberhaft <yossisilb@gmail.com>
2016-05-26 21:15:01 +0300

    Fixed the JSON format problem

--- src/SingleMazeServlet.java
+++ src/SingleMazeServlet.java
@@ -7,20 +7,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InterruptedIOException;
 import java.io.PrintWriter;
 import java.util.Random;
 
 
 @WebServlet(name = "SingleMazeServlet", urlPatterns = {"/SingleMazeServlet"})
 public class SingleMazeServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
 
-    }
 
+    /***********************************************************************
+     * This is the doGet method that will be used in order to get the maze
+     * from the Server
+     * @param request - request received
+     * @param response - response
+     * @throws ServletException -
+     * @throws IOException - for the writer
+     ***********************************************************************/
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        System.out.println("Inside doGet of SingleMazeServlet");
         ServletContext context = getServletContext();
         Object att = context.getAttribute("connection");
         ServerConnectionManager con = (ServerConnectionManager) att;
@@ -30,17 +35,30 @@ public class SingleMazeServlet extends HttpServlet {
         int randNum = rand.nextInt(40);
         String number = Integer.toString(randNum);
         String singleCommand = "1 Game";
-        singleCommand += number + " " + 0;
+        singleCommand += number + " " + 1;
 
         //Sends the command for a new maze to the server
         String mazeFromServer = con.sendToServer(singleCommand);
-        System.out.println("Maze From Server NEW: " + mazeFromServer);
+        String mazeString;
 
-        String mazeString = mazeFromServer.substring(0, 234);
+        //Checks if the JSON received is in the correct format
+        if(mazeFromServer.charAt(233) == '\n' &&
+                mazeFromServer.charAt(234) == '}'){
+            mazeString = mazeFromServer.substring(0, 234);
+            mazeString += "}";
+        } else{
+            mazeString = mazeFromServer.substring(0, 234);
+        }
+
         JSONObject object = new JSONObject();
         object.put("singleMaze", mazeString);
         response.setContentType("application/json");
         PrintWriter out = response.getWriter();
         out.println(object);
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
     }
 }
