471392ab825857d6ea4b6edec30a98936c29d7eb
NAVAJAVA <nshemoul@gmail.com>
2016-05-26 17:55:28 +0300

    FIxed movement

--- web/MazeActions.js
+++ web/MazeActions.js
@@ -1,8 +1,6 @@
-var currX = 470;
-var currY = 100;
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
-var mazeWidth = 870;
+var mazeWidth = 790;
 var mazeHeight = 460;
 var imageObj = document.getElementById("myImg");
 imageObj.style.position= 'relative';
@@ -12,7 +10,7 @@ imageObj.style.top = '100px';
 //imageObj.id = "myImg";
 
 //gets a string and makes a grid out of it.
-function DrawMaze(stringMaze) {
+function DrawMaze(stringMaze, endX, endY) {
  context.rect(430,100,400,400);
  context.stroke();
  var x = 430, y = 100, index =0;
@@ -28,20 +26,17 @@ function DrawMaze(stringMaze) {
      context.fillStyle = "white";
      context.fillRect(x, y, 40, 40);
     }
+   //color end point
+   if(i==9 && j==9){
+    context.fillStyle = "#3045BF";
+    context.fillRect(endX, endY, 40, 40);
+   }
    x = x + 40;
    index++;
   }
   y = y + 40;
   x = 430;
  }
-}
-
-//place the icon of the player on the maze
-function PlacePlayerIcon(currX ,currY) {
- document.getElementById("myImg").style.visibility = "hidden";
- imageObj.style.left = currX + 'px';
- imageObj.style.top =  currY + 'px';
- imageObj.style.visibility = 'visible';
 }
 
 function canMoveTo(destX, destY) {
@@ -70,12 +65,15 @@ function Win(newX, newY) {
 
 function move(e) {
  var movingAllowed;
+ var color, blue;
  e = e || window.event;
   switch (e.keyCode) {
       // arrow up key
    case 38:
-    movingAllowed = canMoveTo(parseInt(imageObj.style.left), parseInt(imageObj.style.top) - 40);
-       if(movingAllowed == 1) {
+   movingAllowed = canMoveTo(parseInt(imageObj.style.left), parseInt(imageObj.style.top) - 40);
+    color = context.getImageData(parseInt(imageObj.style.left), parseInt(imageObj.style.top) - 40, 40, 40);
+    blue = parseInt(color.data[2]);
+    if(movingAllowed == 1 && blue != 255) {
         document.getElementById("myImg").style.visibility = "hidden";
         imageObj.style.top = parseInt(imageObj.style.top) - 40 + 'px';
         imageObj.style.visibility = 'visible';
@@ -84,7 +82,9 @@ function move(e) {
       // arrow left key
    case 37:
     movingAllowed = canMoveTo(parseInt(imageObj.style.left) - 40, parseInt(imageObj.style.top));
-    if(movingAllowed == 1) {
+    color = context.getImageData(parseInt(imageObj.style.left) - 40, parseInt(imageObj.style.top), 40, 40);
+    blue = parseInt(color.data[2]);
+    if(movingAllowed == 1 && blue != 255) {
      document.getElementById("myImg").style.visibility = "hidden";
      imageObj.style.left = parseInt(imageObj.style.left) - 40 + 'px';
      imageObj.style.visibility = 'visible';
@@ -92,8 +92,11 @@ function move(e) {
     }
       // arrow down key
    case 40:
+       alert("6666");
     movingAllowed = canMoveTo(parseInt(imageObj.style.left), parseInt(imageObj.style.top + 40));
-    if(movingAllowed == 1) {
+    color = context.getImageData(parseInt(imageObj.style.left), parseInt(imageObj.style.top) + 40, 40, 40);
+    blue = parseInt(color.data[2]);
+    if(movingAllowed == 1 && blue != 255) {
      document.getElementById("myImg").style.visibility = "hidden";
      imageObj.style.top = parseInt(imageObj.style.top) + 40 + 'px';
      imageObj.style.visibility = 'visible';
@@ -102,7 +105,9 @@ function move(e) {
       // arrow right key
    case 39:
     movingAllowed = canMoveTo(parseInt(imageObj.style.left) + 40, parseInt(imageObj.style.top));
-    if(movingAllowed == 1) {
+    color = context.getImageData(parseInt(imageObj.style.left) + 40, parseInt(imageObj.style.top), 40, 40);
+    blue = parseInt(color.data[2]);
+    if(movingAllowed == 1 && blue != 255) {
      document.getElementById("myImg").style.visibility = "hidden";
      imageObj.style.left = parseInt(imageObj.style.left) + 40 + 'px';
      imageObj.style.visibility = 'visible';
@@ -118,7 +123,7 @@ imageObj.style.visibility = 'visible';
 
 }
 //main
-DrawMaze("1010101110100010101010111010001000101011101010101010101110101001110011110011110011110011110011110011");
+DrawMaze("0010101110100010101010111010001000101011101010101010101110101001110011110011110011110011110011110011",790, 460);
 //PlacePlayerIcon(currX,currY);
 window.addEventListener("keydown", move);
 
