4700a3c5ed471ff90c64733b1aaf76d2f210a479
Yossi Silberhaft <yossisilb@gmail.com>
2016-05-26 21:17:38 +0300

    Initial commit of file

---
+++ src/MultiplayerMazeServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "MultiplayerMazeServlet", urlPatterns = {"/MultiplayerMazeServlet"})
+public class MultiplayerMazeServlet extends HttpServlet {
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        ServletContext context = getServletContext();
+        Object att = context.getAttribute("connection");
+        ServerConnectionManager manager = (ServerConnectionManager)att;
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+}
