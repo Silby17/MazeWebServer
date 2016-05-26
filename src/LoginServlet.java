dbe0006928d678cf6f46749001b9b684eb8eb801
Yossi Silberhaft <yossisilb@gmail.com>
2016-05-26 18:04:38 +0300

    removeed json from lgin

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -1,5 +1,3 @@
-import org.json.simple.JSONObject;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,7 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 
 @WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
@@ -23,7 +20,6 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        JSONObject obj = new JSONObject();
         ServletContext context = getServletContext();
         Object att = context.getAttribute("database");
         DBManager manager = (DBManager)att;
