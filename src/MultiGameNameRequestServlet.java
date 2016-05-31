fdd73e63280f5702a9a14c440c483ec0c6a39318
Yossi Silberhaft <yossisilb@gmail.com>
2016-05-31 12:42:11 +0300

    Initial Commit of files that will get the mulitplayer maze

---
+++ src/MultiGameNameRequestServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "MultiGameNameRequestServlet", urlPatterns = {"/MultiGameNameRequestServlet"})
+public class MultiGameNameRequestServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String gameName = request.getParameter("gameName");
+        String command = "3 " + gameName;
+        command = command.replaceAll("(\\r|\\n|\\t)", "");
+        User user = (User)request.getSession().getAttribute("user");
+        user.getConnectionManager().sendRequest(command);
+        response.sendRedirect("/Multiplayer.jsp");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+}
