eca35d7c5ce5926ba47dcc85b7ae4446df7ba80b
Yossi Silberhaft <yossisilb@gmail.com>
2016-05-31 23:21:57 +0300

    Getting both mazes working, removed un-used code

--- web/Multiplayer.js
+++ web/Multiplayer.js
@@ -1,33 +1,25 @@
 var current = 0;
-var gameName;
-var checkInterval;
 var mazeFromServer;
 var gameInPlay = false;
-$(function(){
-    $('#getMazeBtn').click(function(){
-        if(gameInPlay == true){
-            if(confirm('Game in Play, are you sure you want to start again?')){
-                //TODO send close command
-            }
-        }
-        if(confirm('Are you sure you want to start a new Game?')){
-            checkInterval = setInterval(function() {getMaze()}, 15000);
-        }
+
+
+$(function start() {
+    $('.loader').show();
+    $.getJSON("MultiplayerMazeServlet", function (data) {
+       if(data.multiMaze != current){
+           mazeFromServer = data.multiMaze;
+           alert("Done");
+           $('.loader').hide();
+           drawBothMazes();
+       }
+        else{
+           start();
+       }
     });
 });
-function getMaze(){
-    gameName = $('#inputMazeName').val();
-    var nameToPass = {mazeName : gameName};
-    $.getJSON("MultiplayerMazeServlet", nameToPass , function(data){
-        if (data.multiMaze != current)
-            mazeFromServer = data.multiMaze;
-        gameInPlay = true;
-        console.log(mazeFromServer);
-        stopJSONCheck();
-    })
-}
-function stopJSONCheck() {
-    clearInterval(checkInterval);
-    var ob = JSON.parse(mazeFromServer);
-    console.log(ob);
-}
+
+
+function drawBothMazes(){
+    var object = JSON.parse(mazeFromServer);
+    console.log(object);
+};
