21fe1fbd8de38963457ede58ea5693fe787512c4
Yossi Silberhaft <yossisilb@gmail.com>
2016-06-02 12:41:47 +0300

    Initial commit

---
+++ src/SendMoveServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Yossi on 02-Jun-16.
+ */
+@WebServlet(name = "SendMoveServlet")
+public class SendMoveServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
