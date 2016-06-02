a6128ec918ee57f2b473f93d4d735e1e9faa5e03
Yossi Silberhaft <yossisilb@gmail.com>
2016-06-02 11:32:56 +0300

    Added in runnable thread

--- src/GetPlayerMoveServlet.java
+++ src/GetPlayerMoveServlet.java
@@ -1,3 +1,5 @@
+import org.json.simple.JSONObject;
+
 import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,6 +14,7 @@ public class GetPlayerMoveServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        System.out.println("Starting doGet of getMOve");
         boolean asyncSupported = request.isAsyncSupported();
         request.setAttribute("org.apache.catalina.ASYNC_SUPPORTED", true);
         asyncSupported = request.isAsyncSupported();
@@ -24,7 +27,6 @@ public class GetPlayerMoveServlet extends HttpServlet {
         }
 
     }
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {}
 }
@@ -38,12 +40,27 @@ class AsyncRunnable implements Runnable{
     }
 
     public void run(){
+        System.out.println("Starting running");
         HttpServletResponse res = (HttpServletResponse)asyncContext.getResponse();
-
+        HttpServletRequest req = (HttpServletRequest)asyncContext.getRequest();
+        User user = (User)req.getSession().getAttribute("user");
+        System.out.println("Got the user from the session");
         PrintWriter out = null;
 
         try{
+            String fromServer = user.getConnectionManager().getMsgFromServer();
+            System.out.println("Got from Server =  " + fromServer);
+            System.out.println();
+            fromServer = fromServer.replaceAll("(\\n|\\r)", "");
+            JSONObject obj = new JSONObject();
+            obj.put("playerMove", fromServer);
+            out = res.getWriter();
+            out.println(obj);
+            res.setContentType("application/json");
+            asyncContext.complete();
 
+        }catch(IOException e){
+            e.printStackTrace();
         }
 
     }
