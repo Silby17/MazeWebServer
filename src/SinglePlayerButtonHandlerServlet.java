e7448178df1f8970bd7cf87b31e4f912b6f91306
Yossi Silberhaft <yossisilb@gmail.com>
2016-06-02 15:38:52 +0300

    FINAL COMMIT - all working

---
+++ src/SinglePlayerButtonHandlerServlet.java
@@ -0,0 +1,38 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/****************************************************************************
+ * This class will handle button clicks on the SinglePlayer window
+ ****************************************************************************/
+@WebServlet(name = "SinglePlayerButtonHandlerServlet",
+        urlPatterns = {"/SinglePlayerButtonHandlerServlet"})
+public class SinglePlayerButtonHandlerServlet extends HttpServlet {
+
+    /*************************************************************************
+     * This method will receive the post from the client
+     * @param request - from client
+     * @param response - to send
+     * @throws ServletException - throws exception
+     * @throws IOException
+     *************************************************************************/
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        //Gets the Button value that was posted
+        String buttonClick = request.getParameter("action");
+
+        //Checks if the button clicked was to go back to the main menu
+        // if it was then return to the  main menu
+        if(buttonClick.equals("return")){
+            response.sendRedirect("controllers.MenuServlet");
+        }
+    }
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {}
+}
