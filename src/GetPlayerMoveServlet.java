512ccbb98b919250754e887d213820adef97d620
Yossi Silberhaft <yossisilb@gmail.com>
2016-06-02 15:43:18 +0300

    Comments done

--- src/GetPlayerMoveServlet.java
+++ src/GetPlayerMoveServlet.java
@@ -8,18 +8,31 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "GetPlayerMoveServlet", asyncSupported = true, urlPatterns = {"/GetPlayerMoveServlet"})
+
+/*****************************************************************************
+ * This Servlet controller will be in charge of getting the other players move
+ * asynchronously from the server
+ ****************************************************************************/
+@WebServlet(name = "GetPlayerMoveServlet", asyncSupported = true,
+        urlPatterns = {"/GetPlayerMoveServlet"})
 public class GetPlayerMoveServlet extends HttpServlet {
 
+    /************************************************************************
+     * This method will start the running of the Asynchronous thread that will
+     * check if the other player has moved
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     ************************************************************************/
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        System.out.println("Starting doGet of getMOve");
         boolean asyncSupported = request.isAsyncSupported();
         request.setAttribute("org.apache.catalina.ASYNC_SUPPORTED", true);
         asyncSupported = request.isAsyncSupported();
 
         if (asyncSupported) {
-            AsyncContext asyncCtx = request.startAsync(request, response);  // req.startAsync();
+            AsyncContext asyncCtx = request.startAsync(request, response);
             asyncCtx.setTimeout(0); // => disable timeout
             AsyncRunnable thread = new AsyncRunnable(asyncCtx);
             asyncCtx.start(thread);
@@ -39,28 +52,27 @@ class AsyncRunnable implements Runnable{
     }
 
     public void run(){
-        System.out.println("Starting running");
         HttpServletResponse res = (HttpServletResponse)asyncContext.getResponse();
         HttpServletRequest req = (HttpServletRequest)asyncContext.getRequest();
         User user = (User)req.getSession().getAttribute("user");
-        System.out.println("Got the user from the session");
         PrintWriter out = null;
 
         try{
             String fromServer = user.getConnectionManager().getMsgFromServer();
-            System.out.println("Got from Server =  " + fromServer);
             System.out.println();
             fromServer = fromServer.replaceAll("(\\n|\\r)", "");
+
+            int indexOfEnd = fromServer.indexOf('}');
+            fromServer = fromServer.substring(0, indexOfEnd + 1);
+
             JSONObject obj = new JSONObject();
             obj.put("playerMove", fromServer);
             out = res.getWriter();
             out.println(obj);
             res.setContentType("application/json");
             asyncContext.complete();
-
         }catch(IOException e){
             e.printStackTrace();
         }
-
     }
 }
